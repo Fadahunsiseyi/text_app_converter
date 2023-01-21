4042b69c778f3747bc04dd63da2c3fd2491535b4
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2023-01-21 13:04:06 +0100

    import function to handle inputs

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-import './index.css'
-import "@fortawesome/fontawesome-free/css/all.min.css";
+import './index.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import handleInput from './modules/handleInput';
 
-console.log('object')
+console.log('object');
+
+handleInput();
